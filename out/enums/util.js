0b5ba2ccbd94214c40835375f0b8534f3019f172
Lorran David <lorrantd1@gmail.com>
2022-02-01 16:28:29 -0300

    Remove unecessary folder /out/ from repo

--- out/enums/util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UtilEnum;
-(function (UtilEnum) {
-    UtilEnum["PLACEHOLDER"] = "__PLACEHOLDER__";
-})(UtilEnum = exports.UtilEnum || (exports.UtilEnum = {}));
-//# sourceMappingURL=util.js.map
